0afaa40becd32a817ef4a34026702a4adab4b55b
Jason Shouldice <jason@ccdocs.com>
2026-04-04 20:15:53 -0500

    Initial commit: Asterisk Manager Interface (AMI): The Complete Developer Guide

---
+++ src/AsteriskManagerInterfaceGuide.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Asterisk Manager Interface (AMI): The Complete Developer Guide
+ *
+ * @see https://vicistack.com/blog/asterisk-manager-interface-guide/
+ * @package vicistack/asterisk-manager-interface-guide
+ */
+namespace ViciStack\AsteriskManagerInterfaceGuide;
+
+class AsteriskManagerInterfaceGuide
+{
+    /**
+     * @return string Documentation URL
+     */
+    public static function getDocumentationUrl(): string
+    {
+        return 'https://vicistack.com/blog/asterisk-manager-interface-guide/';
+    }
+
+    /**
+     * @return string Package topic
+     */
+    public static function getTopic(): string
+    {
+        return 'Asterisk Manager Interface (AMI): The Complete Developer Guide';
+    }
+}
